c261b06bbb59b09f042993a2ce9d7070ebb51a65
Daniel Santhiago <danieldeveloper01@gmail.com>
2024-06-02 14:14:26 -0300

    change data test

--- dags/postgres_to_bigquery.py
+++ dags/postgres_to_bigquery.py
@@ -25,7 +25,7 @@ import decimal
 default_args = {
     'owner': 'daniel',
     'depends_on_past': False,
-    'start_date': datetime(2024, 1, 1),
+    'start_date': datetime(2024, 2, 1),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 0,
